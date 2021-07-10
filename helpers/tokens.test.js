722349275a2e76c82944ac18f81b52c4f73b544f
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-07-10 14:24:13 -0500

    Update tokens test cases

--- helpers/tokens.test.js
+++ helpers/tokens.test.js
@@ -3,12 +3,21 @@ const { createToken } = require("./tokens");
 const { SECRET_KEY } = require("../config");
 
 describe("createToken", function () {
-  test("works: not admin", function () {
+  test("Can create a user token with no errors", function () {
     const token = createToken({ username: "test" });
     const payload = jwt.verify(token, SECRET_KEY);
     expect(payload).toEqual({
       iat: expect.any(Number),
       username: "test"      
     });
-  });  
+  });
+
+  test("Can create a company token with no errors", function () {
+    const token = createToken({ companyHandle: "test" });
+    const payload = jwt.verify(token, SECRET_KEY);
+    expect(payload).toEqual({
+      iat: expect.any(Number),
+      companyHandle: "test"
+    });
+  }); 
 });
