0234c3fc2c3ad4fc58465533f6e473af8913ca6c
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-07-25 13:21:37 -0500

    Update seed file

--- volunteer-seed.sql
+++ volunteer-seed.sql
@@ -38,7 +38,7 @@ Phasellus in felis. Donec semper sapien a libero. Nam dui.
 
 Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.
 
-Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/d352rWK.png', 'Database Administrator IV', 'company');
+Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/jRSWh9o.png', 'Database Administrator IV', 'company');
 insert into companies (company_handle, password, company_name, state, num_employees, short_description, long_description, website_url, logo_url, main_image_url, looking_for, role) values ('gogl', 'EUc3WomzHlbz', 'Runolfsson-Wyman', 'Oregon', 792, 'viverra eget congue eget', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.
 
 Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
@@ -52,7 +52,7 @@ Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
 
 Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
 
-Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/d352rWK.png', 'Legal Assistant', 'company');
+Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/jRSWh9o.png', 'Legal Assistant', 'company');
 insert into companies (company_handle, password, company_name, state, num_employees, short_description, long_description, website_url, logo_url, main_image_url, looking_for, role) values ('appl', 'pu6WlkIrv', 'Mante, Rippin and Nikolaus', 'Georgia', 951, 'eu nibh quisque id justo', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.
 
 Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
@@ -66,7 +66,7 @@ Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, sempe
 
 Phasellus in felis. Donec semper sapien a libero. Nam dui.
 
-Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.', 'http://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/d352rWK.png', 'Account Representative I', 'company');
+Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.', 'http://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/jRSWh9o.png', 'Account Representative I', 'company');
 insert into companies (company_handle, password, company_name, state, num_employees, short_description, long_description, website_url, logo_url, main_image_url, looking_for, role) values ('grow', 'g0xXQ7b0RHS', 'Barton-Barton', 'New York', 703, 'leo maecenas pulvinar lobortis est', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.
 
 Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
@@ -80,21 +80,21 @@ Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
 
 Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.
 
-Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/d352rWK.png', 'Account Coordinator', 'company');
+Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/ZGz1JvZ.png', 'Account Coordinator', 'company');
 insert into companies (company_handle, password, company_name, state, num_employees, short_description, long_description, website_url, logo_url, main_image_url, looking_for, role) values ('zayo', 'hthVRI9LqaD', 'Yost Inc', 'Louisiana', 521, 'amet cursus id turpis integer', 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.
 
 Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.
 
 Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.
 
-Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.', 'google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/d352rWK.png', 'Speech Pathologist', 'company');
+Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.', 'google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/jRSWh9o.png', 'Speech Pathologist', 'company');
 insert into companies (company_handle, password, company_name, state, num_employees, short_description, long_description, website_url, logo_url, main_image_url, looking_for, role) values ('bofi', 'KbNRSF9TbLhy', 'Rippin-Cormier', 'District of Columbia', 914, 'morbi odio odio elementum', 'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.
 
 Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.
 
 Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
 
-Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/d352rWK.png', 'Internal Auditor', 'company');
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/ZGz1JvZ.png', 'Internal Auditor', 'company');
 insert into companies (company_handle, password, company_name, state, num_employees, short_description, long_description, website_url, logo_url, main_image_url, looking_for, role) values ('cohf', 'Z99S6zQ', 'Greenholt, Weimann and Greenfelder', 'New York', 507, 'mauris eget massa tempor', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.
 
 Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.
@@ -115,7 +115,7 @@ Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pu
 
 Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.
 
-Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/d352rWK.png', 'Administrative Officer', 'company');
+Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/jRSWh9o.png', 'Administrative Officer', 'company');
 insert into companies (company_handle, password, company_name, state, num_employees, short_description, long_description, website_url, logo_url, main_image_url, looking_for, role) values ('wsry', 'FqZTV5i', 'Lang-Hintz', 'Arizona', 956, 'mus vivamus vestibulum sagittis', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.
 
 Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
@@ -129,7 +129,7 @@ Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed 
 
 Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
 
-Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/d352rWK.png', 'Editor', 'company');
+Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/ZGz1JvZ.png', 'Editor', 'company');
 insert into companies (company_handle, password, company_name, state, num_employees, short_description, long_description, website_url, logo_url, main_image_url, looking_for, role) values ('perf', 'XgZCn47B', 'Gislason, Halvorson and McDermott', 'Kentucky', 654, 'odio porttitor id consequat', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
 
 Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
@@ -143,14 +143,14 @@ Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
 
 Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
 
-Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/d352rWK.png', 'Project Manager', 'company');
+Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/jRSWh9o.png', 'Project Manager', 'company');
 insert into companies (company_handle, password, company_name, state, num_employees, short_description, long_description, website_url, logo_url, main_image_url, looking_for, role) values ('carb', '1W4G6xsiiI2J', 'Moen-D''Amore', 'Illinois', 558, 'ante vestibulum ante ipsum primis', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
 
 Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
 
 In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.
 
-Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/d352rWK.png', 'Design Engineer', 'company');
+Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/ZGz1JvZ.png', 'Design Engineer', 'company');
 insert into companies (company_handle, password, company_name, state, num_employees, short_description, long_description, website_url, logo_url, main_image_url, looking_for, role) values ('bidu', 'zUV9ZVW', 'Friesen, Abbott and Treutel', 'District of Columbia', 640, 'varius integer ac leo', 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.
 
 In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.
@@ -164,4 +164,4 @@ Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
 
 Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
 
-Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/d352rWK.png', 'Quality Control Specialist', 'company');
+Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 'https://google.com', 'https://i.imgur.com/nuSnmLm.jpg', 'https://i.imgur.com/jRSWh9o.png', 'Quality Control Specialist', 'company');
